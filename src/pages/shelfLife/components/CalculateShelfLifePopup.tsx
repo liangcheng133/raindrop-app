12f8954006c4a54b9cdb9b9eaf4b21922eeaa86a
asin_CLX <asin2002@163.com>
2025-02-25 15:28:30 +0800

    feat(shelfLife): 添加计算保质期弹框并优化表单
    - 新增 CalculateShelfLifePopup 组件，用于计算保质期
    - 优化表单布局，使用 Field 组件替代原有的 Form.Item 和 Cell 组件
    - 添加打开计算保质期弹框的功能
    - 调整样式

--- src/pages/shelfLife/components/CalculateShelfLifePopup.tsx
+++ src/pages/shelfLife/components/CalculateShelfLifePopup.tsx
@@ -1,17 +1,35 @@
 import { Popup } from '@taroify/core'
 import { ScrollView } from '@tarojs/components'
 import { useSafeState } from 'ahooks'
-import React from 'react'
+import { forwardRef, useImperativeHandle } from 'react'
+import '../index.less'
+
+export interface CalculateShelfLifePopupProps {}
+
+export interface CalculateShelfLifePopupRef {
+  open: () => void
+  close: () => void
+}
 
 /** 计算保质期弹框 */
-const CalculateShelfLifePopup: React.FC<React.PropsWithChildren> = (props) => {
+const CalculateShelfLifePopup = forwardRef<CalculateShelfLifePopupRef, CalculateShelfLifePopupProps>((props, ref) => {
   const [visible, setVisible] = useSafeState(false)
 
+  const open = () => {
+    setVisible(true)
+  }
+
+  const close = () => {
+    setVisible(false)
+  }
+
+  useImperativeHandle(ref, () => ({ open, close }), [])
+
   return (
-    <Popup open={visible} lock rounded>
+    <Popup className='calculate-shelf-life-popup' open={visible} lock rounded>
       <ScrollView>内容</ScrollView>
     </Popup>
   )
-}
+})
 
 export default CalculateShelfLifePopup
--- src/pages/shelfLife/index.tsx
+++ src/pages/shelfLife/index.tsx
@@ -1,50 +1,48 @@
 import { Layout } from '@/components'
-import { Button, Cell, Form, Input, Toast } from '@taroify/core'
+import { Button, Cell, Field, Form, Input } from '@taroify/core'
 import { View } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import React from 'react'
+import CalculateShelfLifePopup, { CalculateShelfLifePopupRef } from './components/CalculateShelfLifePopup'
 import './index.less'
 
 const ShelfLifeIndex: React.FC<React.PropsWithChildren> = (props) => {
-  const onTest = () => {
-    Taro.navigateTo({ url: '/pages/index/index' })
+  const calculateShelfLifePopupRef = React.createRef<CalculateShelfLifePopupRef>()
+
+  // 打开计算保质期弹窗
+  const openCalculateShelfLifePopup = () => {
+    calculateShelfLifePopupRef.current?.open()
   }
+
   return (
     <Layout>
       <Form controlAlign='right'>
-        <Toast id='toast' />
-        <Form.Item name='id' noStyle />
+        <Form.Item name='id' noStyle></Form.Item>
         <View className='header-title'>基础信息</View>
         <Cell.Group inset>
-          <Form.Item name='name' required rules={[{ required: true, message: '请填写物品名称' }]}>
-            <Form.Label>物品名称</Form.Label>
-            <Form.Control>
-              <Input placeholder='物品名称' />
-            </Form.Control>
-          </Form.Item>
+          <Field label='物品名称' name='name' required rules={[{ required: true, message: '请填写物品名称' }]}>
+            <Input placeholder='物品名称' />
+          </Field>
         </Cell.Group>
 
         <View className='header-title'>保质期</View>
         <Cell.Group inset>
-          <Form.Item name='produced_time'>
-            <Form.Label>生产日期</Form.Label>
-            <Form.Control>
-              <Input placeholder='生产日期' />
-            </Form.Control>
-          </Form.Item>
-          <Form.Item name='expiration_time' required rules={[{ required: true, message: '请填写过期时间' }]}>
-            <Form.Label>过期时间</Form.Label>
-            <Form.Control>
-              <Input placeholder='过期时间' />
-            </Form.Control>
-          </Form.Item>
-          <Cell title={<View></View>}>
-            <Button variant='text' color='primary' size='small'>
-              计算保质期
-            </Button>
-          </Cell>
+          <Field label='生产日期' name='produced_time'>
+            <Input placeholder='生产日期' />
+          </Field>
+          <Field
+            label='过期时间'
+            name='expiration_time'
+            required
+            isLink
+            clickable
+            rules={[{ required: true, message: '请填写过期时间' }]}>
+            <Input placeholder='过期时间' readonly onClick={openCalculateShelfLifePopup} />
+          </Field>
         </Cell.Group>
       </Form>
+
+      <CalculateShelfLifePopup ref={calculateShelfLifePopupRef} />
     </Layout>
   )
 }
