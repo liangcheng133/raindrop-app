bc3ed9725599004f0a8ae4e78cbd306183feb313
asin_CLX <asin2002@163.com>
2025-03-25 18:05:42 +0800

    feat: 添加访客用户登录页面
    - 在 app.config.ts 中添加了访客用户登录页面的路由
    - 新增页面路径为 'pages/guestUser/login'

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,6 @@
 export default defineAppConfig({
   pages: [
+    'pages/guestUser/login',
     'pages/shelfLife/manage',
     'pages/shelfLife/index',
   ],
---
+++ src/pages/guestUser/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+/** 游客用户登录 */
+const GuestUserLogin: React.FC<React.PropsWithChildren> = (props) => {
+  return <div> 这是 GuestUserLoginsss 组件 </div>
+}
+
+export default GuestUserLogin
